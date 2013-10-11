8d8b958c71d12bbea6553eca51fdeacc54c0f777
Pishty <pishty1@gmail.com>
2013-10-11 22:46:26 +0100

    nicer output

--- src/hello_comp/views/displays.clj
+++ src/hello_comp/views/displays.clj
@@ -3,21 +3,6 @@
         [hiccup.form :only (form-to label text-field submit-button check-box radio-button)])
   (:require [hello_comp.views.layout :as layout]))
 
-(defn headerTempl [title]
- [:head
-  [:title title]])
-
-
-(defn index-page []
-  (html5
-    [:html {:ng-app ""}
-     (headerTempl "PicMi")
-     [:body
-      [:div
-       [:label "Name :"]
-       [:input {:type "text" :ng-model "yourName" :placeholder "Enter a name here"}]
-       [:h1 "Hello {{yourName}} !"]]]]))
-
 
 (defn skill-label [skill level value checked?]
   (label {:class "radio inline"} (str skill "-" level)
@@ -27,11 +12,11 @@
 (defn render-row [skill]
   [:div {:class "control-group"}
      (label {:class "control-label"} skill skill)
-     [:div {:class "controls"}
+     [:div  {:class "controls"}
       (map skill-label [skill    skill     skill         skill]
                        ["noobie" "alright" "pretty good" "call me Gandalf"]
                        [1        2         3             4]
-                       [true    false     false         false])]])
+                       [true    false     false          false])]])
 
 
 (defn skill-radio []
@@ -40,9 +25,8 @@
     [:label "Skills Form"]
     (map render-row ["Clojure" "Scala" "Java" "JBoss"])
     [:div {:class "control-group"}
-     (label {:class "control-label"} "singlebutton" "")
      [:div {:class "controls"}
-      (submit-button {:id "singlebutton" :name "singlebutton" :class "btn btn-primary"} "SKILLS")]]]))
+      (submit-button {:id "singlebutton" :class "btn btn-primary"} "SKILLS")]]]))
 
 
 (defn show-form-simple []
