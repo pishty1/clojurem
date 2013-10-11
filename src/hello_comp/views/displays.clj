fbffb1f9f24898c4d6b4af610252ca1cfc5f47d6
Pishty <pishty1@gmail.com>
2013-10-11 19:38:02 +0100

    further refinement to the radio form

--- src/hello_comp/views/displays.clj
+++ src/hello_comp/views/displays.clj
@@ -19,21 +19,23 @@
        [:h1 "Hello {{yourName}} !"]]]]))
 
 
-(defn skill-label [skill level]
+(defn skill-label [skill level value checked?]
   (label {:class "radio inline"} (str skill "-" level)
-         (conj (radio-button {:id (str skill "-" level)} skill false  "1") level)))
+         (conj (radio-button {:id (str skill "-" level)} skill checked?  value) level)))
 
-;;this function now works but dont like the way the parameters are passed in, skill is coppied everywhere
+;;this function now works but dont like the way the parameters are passed in
 (defn render-row [skill]
   [:div {:class "control-group"}
      (label {:class "control-label"} skill skill)
      [:div {:class "controls"}
       (map skill-label [skill    skill     skill         skill]
-                       ["noobie" "alright" "pretty good" "call me Gandalf"])]])
+                       ["noobie" "alright" "pretty good" "call me Gandalf"]
+                       [1        2         3             4]
+                       [true    false     false         false])]])
 
 
 (defn skill-radio []
-  (form-to {:class "form-horizontal"}[:post "/"]
+  (form-to {:class "form-horizontal"}[:get "/"]
    [:fieldset
     [:label "Skills Form"]
     (map render-row ["Clojure" "Scala" "Java" "JBoss"])
