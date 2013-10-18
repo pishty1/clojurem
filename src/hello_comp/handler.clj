dc0dece0536cf3f57fb1917f5c8b6962098c0b5f
Thierno Diallo <dialoumar84@yahoo.fr>
2013-10-18 01:42:41 +0100

    Adds ability to register user in mongodb (work in Progress)

--- src/hello_comp/handler.clj
+++ src/hello_comp/handler.clj
@@ -9,43 +9,42 @@
             [compojure.route :as route]
             [monger.core :as mg]))
 
-;; using MongoOptions allows fine-tuning connection parameters,
-;; like automatic reconnection (highly recommended for production environment)
-(let [^MongoOptions opts (mg/mongo-options :threads-allowed-to-block-for-connection-multiplier 300)
-      ^ServerAddress sa  (mg/server-address "127.0.0.1" 27017)]
-  (mg/connect! sa opts))
-
-
-(defroutes app-routes
-  (GET "/" {session :session}
+	;; using MongoOptions allows fine-tuning connection parameters,
+	;; like automatic reconnection (highly recommended for production environment)
+	(let [^MongoOptions opts (mg/mongo-options :threads-allowed-to-block-for-connection-multiplier 300)
+		  ^ServerAddress sa  (mg/server-address "127.0.0.1" 27017)]
+	  (mg/connect! sa opts))
+	  
+	  
+	  (mg/set-db! (mg/get-db "clojurem-db"))
+  
+(defroutes app-routes [mg]
+   (GET "/" {session :session}
       (let [count   (:count session 0)
         session (assoc session :count (inc count))]
     (-> (response (indexpage count))
         (assoc :session session))))
 
-  (GET ["/skillform"] {{jb :JBoss jv :Java} :params} (str "the jbeezy is " jb " and the javaaa is " jv))
+   (GET ["/skillform"] {{jb :JBoss jv :Java} :params} (str "the jbeezy is " jb " and the javaaa is " jv))
 
-  ;;(GET "/insert" []
-     ;;  (insertDoc)
-      ;; (index-page))
 
-  (GET "/login" []
+   (GET "/login" []
        (loginpage true))
 
-  (GET "/registration/new" []
+   (GET "/registration/new" []
        (createlogin))
+	   
+	(GET "/registerUser" [emailaddress password]
+       (mg/insert "users" { :email_address "emailaddress" :password "password" }))
 
-  (route/resources "/")
-  (route/not-found "Not Found"))
-
+   (route/resources "/")
+   (route/not-found "Not Found"))
 
 (def app
-  (->(handler/site app-routes)
+   (->(handler/site app-routes)
      (wrap-session {:cookie-attrs {:max-age 60 :secure true}})
      (wrap-base-url)))
 
-
-
- ;; not in use might use it as an example
-  (GET ["/user/:id", :id #"[0-9]+"] {{id :id} :params}
+	;; not in use might use it as an example
+    (GET ["/user/:id", :id #"[0-9]+"] {{id :id} :params}
        (str "<h1>Hello user   " id "</h1>"))
