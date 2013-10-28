bf294eb90737f696f6d61d95de82cd658dff5724
Pishty <pishty1@gmail.com>
2013-10-28 22:52:03 +0000

    made the form look nicer

--- src/hello_comp/views/displays.clj
+++ src/hello_comp/views/displays.clj
@@ -5,27 +5,27 @@
   (:require [hello_comp.views.layout :as layoutT]))
 
 (defn skill-label [skill level value checked?]
-  (label {:class "radio inline"} (str skill "-" level)
+  (label {:class "radio-inline"} (str skill "-" level)
          (conj (radio-button {:id (str skill "-" level)} skill checked?  value) level)))
 
 ;;this function now works but dont like the way the parameters are passed in
 (defn render-row [skill]
-  [:div {:class "control-group"}
-     (label {:class "control-label"} skill skill)
-     [:div  {:class "controls"}
+  [:div {:class "form-group"}
+     (label {:class "col-md-2 control-label"} skill skill)
+     [:div  {:class "col-md-10 controls"}
       (map skill-label [skill    skill     skill         skill]
                        ["noobie" "alright" "pretty good" "call me Gandalf"]
                        [1        2         3             4]
                        [true     false     false         false])]])
 
 (defn show-form-simple []
-  (layoutT/common (form-to {:class "form-horizontal"}[:get "/skillform"]
+  (layoutT/common (form-to {:class "form-horizontal" :role "form"}[:get "/skillform"]
    [:fieldset
     [:label "Skills Form"]
     (map render-row ["Clojure" "Scala" "Java" "JBoss"])
     [:div {:class "control-group"}
      [:div {:class "controls"}
-      (submit-button {:id "singlebutton" :class "btn btn-primary"} "SKILLS")]]])))
+      (submit-button {:id "singlebutton" :class "col-md-offset-2 btn btn-primary"} "SKILLS")]]])))
 
 (defn indexpage [counter]
   (layoutT/common(html5 [:div (str "hello " counter)])))
